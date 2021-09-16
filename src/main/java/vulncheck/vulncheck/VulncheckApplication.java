aab197bbbf9b615669889ca6f88b1f764890a5f9
Sam Bultitude <sam.bultitude@gamma.co.uk>
2021-09-16 11:51:34 +0100

    c

---
+++ src/main/java/vulncheck/vulncheck/VulncheckApplication.java
@@ -0,0 +1,13 @@
+package vulncheck.vulncheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VulncheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VulncheckApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vulncheck/vulncheck/VulncheckApplicationTests.java
@@ -0,0 +1,13 @@
+package vulncheck.vulncheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VulncheckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
